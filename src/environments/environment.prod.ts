2e305e4bfe77d694318407e18b3c115578398df0
pts86 <68948373+pts86@users.noreply.github.com>
2025-07-29 00:31:04 -0700

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  /**
+   * Production API URL for the Guacamole REST API. Adjust as necessary
+   * to reflect the deployed host and port.
+   */
+  apiUrl: 'http://localhost:8085/guacamole/api',
+  username: 'guacadmin',
+  password: 'guacadmin'
+};
